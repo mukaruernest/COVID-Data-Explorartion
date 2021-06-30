798d6cd8a9a8c014676e4fb94afe4b8d77a00cc1
Ernest Mukaru <mukaruernesto@gmail.com>
2021-07-01 01:06:21 +0300

    covid19 data exploration structure

---
+++ coviddataanalysis.sql
@@ -0,0 +1,36 @@
+-- Select Data that we are going to be starting with
+
+
+-- Total Cases vs Total Deaths
+-- Shows likelihood of dying if you contract covid in your country
+
+
+-- Total Cases vs Population
+-- Shows what percentage of population infected with Covid
+
+
+-- Countries with Highest Infection Rate compared to Population
+
+
+-- Countries with Highest Death Count per Population
+
+
+-- BREAKING THINGS DOWN BY CONTINENT
+
+-- Showing contintents with the highest death count per population
+
+
+-- GLOBAL NUMBERS
+
+
+-- Total Population vs Vaccinations
+-- Shows Percentage of Population that has recieved at least one Covid Vaccine
+
+
+-- Using CTE to perform Calculation on Partition By in previous query
+
+
+-- Using Temp Table to perform Calculation on Partition By in previous query
+
+
+-- Creating View to store data for later visualizations
